f0de5c6b4dd04fbbc09b1c169f7cf4dacee7590b
Pat <113007904+PattZac@users.noreply.github.com>
2023-01-22 15:25:37 +0100

    Add files via upload

---
+++ varPzpz1p4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varPzpz1p4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int x = Convert.ToInt32(s.Substring(0, s.IndexOf(' '))),
+            y = Convert.ToInt32(s.Substring(
+                s.IndexOf(' '), s.Length - s.IndexOf(' ')));
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+        }
+    }
+}
